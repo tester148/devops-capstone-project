fd05157d13c9f8de7b71bd6989098b4bd5d49098
tester-148 <tester-148>
2025-02-01 11:01:36 -0500

    read accounts

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -123,4 +123,15 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
+
+
+
+
+
+
     # ADD YOUR TEST CASES HERE ...
+
+    def test_get_account_not_found(self):
+    """It should not Read an Account that is not found"""
+    resp = self.client.get(f"{BASE_URL}/0")
+    self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
